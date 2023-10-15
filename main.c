d737bdd6eca3d50767aa99ba4e87b3563450c1f9
Mattan Mohel <mattanmohel@gmail.com>
2023-10-15 15:39:01 -0400

    created non-homogenous growable array

--- main.c
+++ main.c
@@ -11,70 +11,15 @@
 #define MAX_TOKEN_SIZE  256
 
 int main(int argc, char** argv) {
-  int* arr = VEC_NEW(int, 1);
-  int i = 99;
-  VEC_ADD(arr, &i); 
-  VEC_ADD(arr, &i);
+  float* vec = VEC_NEW(float, 1);
+  VEC_PUSH(vec, 1.1);
+  VEC_PUSH(vec, 1.2);
+  VEC_PUSH(vec, 1.3);
 
-  printf("%d, %d, %d\n", arr[0], VEC_LEN(arr), VEC_CAP(arr));
+  for (int i = 0; i < VEC_LEN(vec); i++) {
+    printf("element %d is %f\n", i, vec[i]);
+  }
 
   return 0;
 }
-
-enum TokenType {
-  NUMBER,
-  COMMA,
-  COLON,
-  SPACE,
-};
-
-struct Token {
-  enum TokenType type;
-  const char* src;
-};
-  
-void tokenize (const char* src) {
- // struct Vec* toks = VEC(struct Token, 256);
-  char* lex_buf = (char*)malloc(MAX_TOKEN_SIZE);
-  int lex_ptr = 0; 
-
-  memset(lex_buf, 0, MAX_TOKEN_SIZE);
-  
-  int len = strlen(src);
-  for (int i = 0; i < len; i++) {
-    bool is_delimeter = false;
-    for (int j = 0; j < DELIMETER_COUNT; j++) {
-      if (src[i] == DELIMETERS[j]) {
-        is_delimeter = true;
-        break;
-      }
-    }
-
-    if (is_delimeter) {
-      struct Token tok;
-      switch (src[i]) {
-        case ' ':
-          tok.type = SPACE;
-          break;
-        case ',':
-          tok.type = COMMA;
-          break;
-        case ':':
-          tok.type = COLON;
-          break;
-      }
-
-     // vec_push(toks, &tok);
-
-      printf("%s", lex_buf);
-      memset(lex_buf, 0, MAX_TOKEN_SIZE);
-      lex_ptr = 0;
-    } else {
-      lex_buf[lex_ptr] = src[i];
-      lex_ptr++;
-    }
-  }
-
-  free(lex_buf);
-} 
 
--- vec.h
+++ vec.h
@@ -10,10 +10,14 @@
 // |  n   |  nth element | <- NOTE: nth byte depends on array stride 
 //  ---------------------
 
-#define VEC_NEW(T, N) (T*)vec_new(N, sizeof(T))
-#define VEC_ADD(V, E) vec_add((void**)&V, (void*)E, sizeof(V[0]))
+#define DEBUG_VEC 0
+
 #define VEC_LEN(V) ((int*)V)[-1]
 #define VEC_CAP(V) ((int*)V)[-2]
+#define VEC_LOC(V) &((int*)V)[-2]
+
+#define VEC_NEW(T, N) (T*)vec_new(N, sizeof(T))
+#define VEC_PUSH(V, E) vec_realloc((void**)&V, sizeof(V[0])); *(V + VEC_LEN(V)++) = E
 
 void* vec_new (int cap, int stride) {
   int* buf = (int*)malloc(cap * stride + 2 * sizeof(int));
@@ -22,15 +26,17 @@ void* vec_new (int cap, int stride) {
   return (void*)(buf + 2);
 }
 
-void vec_add (void** buf, void* elem, int stride) {
+void* vec_realloc(void** buf, int stride) {
   if (VEC_LEN(*buf) + 1 >= VEC_CAP(*buf)) {
-    VEC_CAP(*buf) *= 2;
-    int* tmp = (int*)realloc(&VEC_CAP(*buf), VEC_CAP(*buf) * stride + 2 * sizeof(int));
-    *buf = (void*)(tmp + 2);
+#if DEBUG_VEC
+    printf("reallocating vec buffer...\n");
+#endif
+    VEC_CAP(*buf) = 2 * (VEC_CAP(*buf) + 1);
+    buf = realloc(VEC_LOC(*buf), VEC_CAP(*buf) * stride + 2 * sizeof(int));
+    buf += 2 * sizeof(int);
   }
 
-  memcpy(*buf + VEC_LEN(*buf) * stride, elem, stride);
-  VEC_LEN(*buf)++;
+  return buf;
 }
 
 #endif
