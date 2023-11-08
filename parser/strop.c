47b656c12ef7f34ffb968aec11b0d0e75096dc47
Mattan Mohel <mattanmohel@gmail.com>
2023-11-07 20:55:35 -0500

    added strop

---
+++ parser/strop.c
@@ -0,0 +1,7 @@
+#include "strop.h"
+#include <string.h>
+
+char *pc_strcpy (const char *str) {
+  char *buf = (char*)malloc(strlen(str) + 1);
+  return strcpy(buf, str);
+}
---
+++ parser/strop.h
@@ -0,0 +1,6 @@
+#ifndef _STROP_H_
+#define _STROP_H_
+
+char *pc_strcpy (const char *str);
+
+#endif
